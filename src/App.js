f5a06923892e74a279443c608033c2088492d7c7
joaokeuwe <joaov2872@hotmail.com>
2022-11-22 21:16:17 -0300

    feat: :fire: importando componentes de slide, e card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import CardProducts from "./components/CardProducts";
+import CarouselProducts from "./components/Carousel";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CardProducts />
+      <CarouselProducts />
     </div>
   );
 }
