8cf6ee958cc3f7a43e7e01fda8afdc25652d8961
joaokeuwe <joaov2872@hotmail.com>
2022-11-22 21:17:26 -0300

    feat: :fire: editando componente header, colocando logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from '../images/logo.png'
 import { Link } from 'react-scroll'
+import Button from 'react-bootstrap/Button';
 
 function CardProducts() {
   return (
@@ -13,7 +14,7 @@ function CardProducts() {
         <li><Link to='#'>Contato</Link> </li>
         <li><Link to='#'>Login</Link> </li>
       </ul>
-
+      <Button variant="outline-primary">Primary</Button>{' '}
     <input type="text" placeholder='categoria' />
     <button>profile</button>
     <button>cart</button>
